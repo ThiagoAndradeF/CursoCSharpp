d304ce63224a8999a2d4b266e6201b326bfab59a
Thiago Andrade <90008244+ThiagoAndradeF@users.noreply.github.com>
2022-04-11 17:07:39 -0300

    CURSO

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CursoCSharp.Fundamentos;
+
+namespace CursoCSharp {
+    class Program {
+        static void Main(string[] args) {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
+                {"Comentarios - Fundamentos", Comentarios.Executar},
+                {"Variáveis e Constantes - Fundamentos", VariaveisEConstantes.Executar},
+                {"Inferência- Fundamentos", Inferencia.Executar},
+                {"Interpolação- Fundamentos", Interpolacao.Executar},
+                {"Notação Ponto- Fundamentos", NotacaoPonto.Executar},
+                {"Lendo Dados do Console- Fundamentos", LendoDados.Executar},
+                {"Formatando Número- Fundamentos", FormatandoNumeros.Executar},
+                {"Converter números- Fundamentos", Conversoes.Executar},
+
+            });
+
+            central.SelecionarEExecutar();
+        }
+    }
+}
