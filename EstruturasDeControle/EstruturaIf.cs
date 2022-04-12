973d421abdd5cdd720a0f5df39e72f3874d7a124
Thiago Andrade <90008244+ThiagoAndradeF@users.noreply.github.com>
2022-04-12 15:31:02 -0300

    Estruras_condicionais-Dia3

---
+++ EstruturasDeControle/EstruturaIf.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CursoCSharp.EstruturasDeControle
+{
+    internal class EstruturaIf
+    {
+        public static void Executar()
+        {
+            Console.WriteLine("Digite sua nota: ");
+            double.TryParse(Console.ReadLine(), out double nota);
+
+            Console.WriteLine("Possui bom comportamento (S/N): ");
+            string entradaComportamento = Console.ReadLine();
+            bool bomComportamento = false;
+            //if (entradaComportamento == "S" || entradaComportamento == "s")
+            //    bomComportamento = true;
+            //bomComportamento= entradaComportamento=="S"|| entradaComportamento=="s";
+            bomComportamento= entradaComportamento.ToLower()=="s";
+
+            if (nota >= 9 && bomComportamento)
+            {
+                Console.WriteLine("Quadro de honra!");
+            }
+
+        }
+    }
+}
